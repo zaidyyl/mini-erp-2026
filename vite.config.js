8b35265a846df82b72ced17b48ecf62ce77acd7f
zaidy <zajazmin@outlook.com>
2026-09-12 22:48:20 -0300

    Configurar ruta base de Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 3000,
-    open: true
+    open: true,
+    base: '/mini-erp-2026/'
   },
   build: {
     outDir: 'dist',
